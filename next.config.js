8b9227afd3d7bbfc15d055d712401f60110c2426
vijayst <vijayst@gmail.com>
2019-03-01 10:06:07 +0530

    get list of items from contentful

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+        CONTENTFUL_SPACE_ID: 'mxki4c5wv2nt',
+        CONTENTFUL_ACCESS_TOKEN:
+            '03b266d198b4550e63657a46a3013943bb3529c88238dc37a7c061c740c95465'
+    }
+};
--- pages/media/index.js
+++ pages/media/index.js
@@ -1,5 +1,32 @@
-export default function Index() {
+import contentful from '../../lib/contentful';
+
+export default function Index(props) {
+    const { items } = props;
+
     return (
-        <h1>Display list of media reviews from Contentful</h1>
+        <div>
+            <h1>Display list of media reviews from Contentful</h1>
+            <ul>
+            {items && items.length ? items.map(item => (
+                <li key={item.slug}>{item.title}</li>
+            )) : null}
+            </ul>
+        </div>
     );
 }
+
+Index.getInitialProps = async () => {
+    const response = await contentful.getEntries({
+        content_type: 'media'
+    });
+    const items = response.items.map(item => {
+        const result = item.fields;
+        result.createdAt = new Date(item.sys.createdAt);
+        result.slug =
+            result.title.toLowerCase().replace(' ', '-') +
+            '-' +
+            item.sys.id;
+        return result;
+    });
+    return { items };
+};
